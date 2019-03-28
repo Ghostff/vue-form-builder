0f3eaa2552fe82f4c3b3e0a7a2ff68b089e5431d
ghostff <frankchris@hotmail.com>
2019-03-27 22:25:16 -0500

    initial commit

---
+++ src/Index.vue
@@ -0,0 +1,84 @@
+<template>
+    <div id="main">
+
+        <div ref="main" class="builder list" style="width:600px;border:1px solid #f00;display:inline-block;min-height:90px;vertical-align:top"></div>
+        <div class="list" style="width:300px;border:1px solid #f00;display:inline-block;vertical-align:top">
+            <div ref="draggable" draggable="true" class="dragable" v-for="(el, i) in list" :data-id="i" :key="el" style="padding:10px;border:1px solid blue">
+                {{ el }}
+            </div>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Index",
+    data() {
+      return {
+        name:'chrys',
+        list: [
+          'text',
+          'file',
+          'email',
+          'number',
+          'hidden',
+          'password',
+          'url',
+          'radio',
+          'range',
+          'select',
+          'country',
+          'textarea',
+          'button',
+          'label',
+          'paragraph',
+          'legend',
+        ],
+        drags: function (e) {
+          console.log(e);
+        }
+      }
+    },
+    mounted: function () {
+
+
+
+      this.$refs.main.addEventListener('drop', e => {
+        // e.preventDefault();
+        console.log(e.dataTransfer.getData('text'));
+      });
+
+      this.$refs.main.addEventListener('ondragover', e => {
+        e.preventDefault();
+      });
+
+      console.log(this.$refs.main);
+
+      this.$refs.draggable.forEach(e => {
+        e.addEventListener('dragstart', e => {
+          e.dataTransfer.setData("text", e.target.getAttribute('data-id'));
+        });
+      });
+
+      this.name = 'sdd'
+
+      // console.log(this.name)
+
+    },
+    methods: {
+      drop(e) {
+        e.preventDefault();
+        console.log(e.target);
+      },
+      drag: function (e) {
+        console.log(e);
+      },
+      allowDrop(ev) {
+        ev.preventDefault();
+      },
+    }
+  }
+</script>
+
---
+++ src/form.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import Index from './Index.vue';
+
+new Vue({
+  components: {Index},
+  el: '#app',
+});
